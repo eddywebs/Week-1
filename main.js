c3f7f2f07a071a3dc8f1d4d2e10ab74bf38a3b49
Adi <arawat@MP-Admins-MacBook-Air.local>
2015-09-02 21:29:32 -0400

    bug fix

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 $(function(){
   var oneUp = function(){
+  	var num = $('#output').html();
     num++;
+
+    $('#output').html(num);
   };
   var printNum = function(){
     var num = 0;
     $('#output').html(num);
   };
   printNum();
+  $('#my-button').click(oneUp);
 });
